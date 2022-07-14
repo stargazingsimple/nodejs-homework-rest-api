7b198a5aa14f214ddad4e5d92a557888d3a1fb44
stargazingsimple <stargazingsimple@gmail.com>
2022-07-14 23:25:06 +0300

    homework-5 completed

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const signUp = require('./signUp');
 const login = require('./login');
 const getCurrent = require('./getCurrent');
 const logout = require('./logout');
+const updateAvatar = require('./updateAvatar');
 
 module.exports = {
     signUp,
     login,
     getCurrent,
     logout,
+    updateAvatar,
 };
